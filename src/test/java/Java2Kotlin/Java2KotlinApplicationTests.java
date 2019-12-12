7c2beb7ffd679f9404cabf60ec4777045a003d2e
Asdren Hoxha <asdren.hoxha@mhp.com>
2019-12-12 09:07:45 +0100

    Initial Commit

---
+++ src/test/java/Java2Kotlin/Java2KotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package Java2Kotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2KotlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
